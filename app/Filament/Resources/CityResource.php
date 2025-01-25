91e43cc65241febf9508e16e3c72b92280a2d3d3
davadaviar <davadaviarsaputra2345@gmail.com>
2025-01-25 19:04:58 +0700

    Feat : Feat action button in city listview data

--- app/Filament/Resources/CityResource.php
+++ app/Filament/Resources/CityResource.php
@@ -62,7 +62,10 @@ class CityResource extends Resource
                 //
             ])
             ->actions([
+
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+                
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
