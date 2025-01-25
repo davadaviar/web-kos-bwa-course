d83fc2561b646ecdb8cb99544b8e79fd8dbb431e
davadaviar <davadaviarsaputra2345@gmail.com>
2025-01-25 19:18:04 +0700

    Feat : Feat category CRUD

---
+++ app/Filament/Resources/CategoryResource.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\CategoryResource\Pages;
+use App\Filament\Resources\CategoryResource\RelationManagers;
+use App\Models\Category;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Str;
+
+class CategoryResource extends Resource
+{
+    protected static ?string $model = Category::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                
+                Forms\Components\FileUpload::make('image')
+                ->image()
+                ->directory('categories')
+                ->columnSpan(2)
+                ->required(),
+
+                Forms\Components\TextInput::make('name')
+                ->maxLength(255)
+                ->reactive()
+                ->afterStateUpdated(
+                    function ($state, callable $set) 
+                    {
+                        $set('slug', Str::slug($state));
+                    }
+                )
+                ->debounce(500)
+                ->required(),
+
+                Forms\Components\TextInput::make('slug')
+                ->required(),
+
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                
+                Tables\Columns\ImageColumn::make('image'),
+                Tables\Columns\TextColumn::make('name'),
+                Tables\Columns\TextColumn::make('slug'),
+                
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListCategories::route('/'),
+            'create' => Pages\CreateCategory::route('/create'),
+            'edit' => Pages\EditCategory::route('/{record}/edit'),
+        ];
+    }
+}
